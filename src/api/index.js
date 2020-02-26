e5ab203a26baebd30f58a48913de4e2b4a8c9b53
b9020748 <f.t.r.rea@newcastle.ac.uk>
2020-02-26 15:09:57 +0000

    First commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "<your key here>",
-  authDomain: "<your key here>",
-  databaseURL: "<your key here>",
-  projectId: "<your key here>",
-  storageBucket: "<your key here>",
-  messagingSenderId: "<your key here>",
-  appId: "<your key here>8"
+  apiKey: "AIzaSyCE8WHVUea86YdLX5DNNLC2NVCG0KU7xpU",
+  authDomain: "finns-12102.firebaseapp.com",
+  databaseURL: "https://finns-12102.firebaseio.com",
+  projectId: "finns-12102",
+  storageBucket: "finns-12102.appspot.com",
+  messagingSenderId: "578376767248",
+  appId: "1:578376767248:web:e64718ee9975215c5c3795",
+  measurementId: "G-JL48CCM4V9"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
